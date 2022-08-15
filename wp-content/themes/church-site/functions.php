fc12bcae3435a368b357b0a7a567bda338777b0a
Илья Бондаренко <bn.ilyaa@yandex.ru>
2022-08-15 22:56:45 +0300

    Позиционирование шапки изменено

--- wp-content/themes/church-site/functions.php
+++ wp-content/themes/church-site/functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function churchSite_scripts() {
-    wp_enqueue_style('style', get_template_directory_uri());
+    wp_enqueue_style('style', get_template_directory_uri() . '/style.css');
     wp_enqueue_style('bootstrap', get_template_directory_uri() . '/dist/css/bootstrap.css');
 }
 
--- wp-content/themes/church-site/index.php
+++ wp-content/themes/church-site/index.php
@@ -8,21 +8,22 @@
     <?php wp_head() ?>
   </head>
 <body>
-<div class="container">
-    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-        <span class="fs-4">Simple header</span>
-      </a>
+  
+  <header class="header">
+    <div class="container">
+      <div class="header__wrapper d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+          <span class="fs-4">Церковь Кропоткина</span>
+        </a>
 
-      <ul class="nav nav-pills">
-        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">About</a></li>
-      </ul>
-    </header>
-</div>
+        <ul class="nav nav-pills">
+          <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Главная</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">Записи служений</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">Контакты</a></li>
+        </ul>
+      </div>
+    </div>
+  </header>
+
 </body>
 </html>
